1cbfd966671374f5e89faa0c1395426296488ea6
Richard Qin <richardqin@Richards-Mac-mini.local>
2021-04-01 12:22:03 -0400

    fix language

--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,4 @@
 import i18next from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from "react-i18next";
 import en from './locales/en/translation.json';
 import cn from './locales/cn/translation.json';
@@ -17,10 +16,9 @@ const resources = {
 
 i18next
     .use(initReactI18next) // passes i18n down to react-i18next
-    .use(LanguageDetector)
     .init({
         resources,
-        // lng: localStorage.getItem('lng')||'cn',
+        lng: localStorage.getItem('lng') ? localStorage.getItem('lng') : navigator.language.includes('zh') ? 'cn' : 'en',
         keySeparator: false, // we do not use keys in form messages.welcome
         interpolation: {
             escapeValue: false // react already safes from xss
