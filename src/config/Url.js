decb70070a2a099dea846dafb17be6ef6faac1aa
Richard Qin <richardqin@Richards-Mac-mini.local>
2021-04-05 16:20:08 -0400

    fixed callback

--- src/config/Url.js
+++ src/config/Url.js
@@ -1,4 +1,4 @@
-export const BASE_API_URL = 'https://swapalltrade.com:20000/api/defi/yield-farming';
+export const BASE_API_URL = 'https://swapallapi.com:20000/api/defi/yield-farming';
 export const CHECK_USER = BASE_API_URL + '/user/sign-up/username/checker?username=';
 export const LOGIN = BASE_API_URL + '/user/metamask/login';
 export const SIGN_UP = BASE_API_URL + '/user/metamask/sign-up';
